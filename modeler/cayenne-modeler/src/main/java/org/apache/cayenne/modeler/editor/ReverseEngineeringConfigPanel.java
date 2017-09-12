cfc44de0b1bf7f1b19659f2a6e1ea96b92bcdcfe
Maxim Petrusevich <maks1464@gmail.com>
2017-09-12 14:12:05 +0300

    CAY-2337 Created draggable DB Structure Tree, update DbImportTab structure

---
+++ modeler/cayenne-modeler/src/main/java/org/apache/cayenne/modeler/editor/ReverseEngineeringConfigPanel.java
@@ -0,0 +1,246 @@
+/*****************************************************************
+ *   Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ ****************************************************************/
+
+package org.apache.cayenne.modeler.editor;
+
+import com.jgoodies.forms.builder.DefaultFormBuilder;
+import com.jgoodies.forms.layout.FormLayout;
+import org.apache.cayenne.dbsync.reverse.dbimport.ReverseEngineering;
+import org.apache.cayenne.map.DataMap;
+import org.apache.cayenne.modeler.ProjectController;
+import org.apache.cayenne.modeler.util.NameGeneratorPreferences;
+import org.apache.cayenne.modeler.util.TextAdapter;
+
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Vector;
+
+/**
+ * @since 4.1
+ */
+public class ReverseEngineeringConfigPanel extends JPanel {
+
+    private static final String DATA_FIELDS_LAYOUT = "right:pref, 3dlu, fill:235dlu";
+
+    private JComboBox<String> strategyCombo;
+    private TextAdapter meaningfulPk;
+    private TextAdapter stripFromTableNames;
+    private TextAdapter defaultPackage;
+    private JCheckBox skipRelationshipsLoading;
+    private JCheckBox skipPrimaryKeyLoading;
+    private JCheckBox forceDataMapCatalog;
+    private JCheckBox forceDataMapSchema;
+    private JCheckBox usePrimitives;
+    private JCheckBox useJava7Types;
+
+    private ProjectController projectController;
+
+    ReverseEngineeringConfigPanel(ProjectController projectController) {
+        this.projectController = projectController;
+        initFormElements();
+        initListeners();
+        buildView();
+    }
+
+    private void buildView() {
+        FormLayout panelLayout = new FormLayout(DATA_FIELDS_LAYOUT);
+        DefaultFormBuilder panelBuilder = new DefaultFormBuilder(panelLayout);
+        panelBuilder.setDefaultDialogBorder();
+
+        panelBuilder.append("Tables with Meaningful PK Pattern:", meaningfulPk.getComponent());
+        panelBuilder.append("Strip from table names:", stripFromTableNames.getComponent());
+        panelBuilder.append("Default package:", defaultPackage.getComponent());
+        panelBuilder.append("Skip relationships loading:", skipRelationshipsLoading);
+        panelBuilder.append("Skip primary key loading:", skipPrimaryKeyLoading);
+        panelBuilder.append("Force datamap catalog:", forceDataMapCatalog);
+        panelBuilder.append("Force datamap schema:", forceDataMapSchema);
+        panelBuilder.append("Use Java primitive types:", usePrimitives);
+        panelBuilder.append("Use old java.util.Data type:", useJava7Types);
+        panelBuilder.append(strategyCombo);
+
+        add(panelBuilder.getPanel());
+    }
+
+    void fillCheckboxes(ReverseEngineering reverseEngineering) {
+        skipRelationshipsLoading.setSelected(reverseEngineering.getSkipRelationshipsLoading());
+        skipPrimaryKeyLoading.setSelected(reverseEngineering.getSkipPrimaryKeyLoading());
+        forceDataMapCatalog.setSelected(reverseEngineering.isForceDataMapCatalog());
+        forceDataMapSchema.setSelected(reverseEngineering.isForceDataMapSchema());
+        usePrimitives.setSelected(reverseEngineering.isUsePrimitives());
+        useJava7Types.setSelected(reverseEngineering.isUseJava7Types());
+    }
+
+    void initializeTextFields(ReverseEngineering reverseEngineering) {
+        meaningfulPk.setText(reverseEngineering.getMeaningfulPkTables());
+        stripFromTableNames.setText(reverseEngineering.getStripFromTableNames());
+        defaultPackage.setText(reverseEngineering.getDefaultPackage());
+    }
+
+    private ReverseEngineering getReverseEngineeringBySelectedMap() {
+        DataMap dataMap = projectController.getCurrentDataMap();
+        return projectController.getApplication().getMetaData().get(dataMap, ReverseEngineering.class);
+    }
+
+    private void initStrategy() {
+        Vector<String> arr = NameGeneratorPreferences
+                .getInstance()
+                .getLastUsedStrategies();
+        strategyCombo.setModel(new DefaultComboBoxModel<>(arr));
+    }
+
+    private void initFormElements() {
+        strategyCombo = new JComboBox<>();
+        strategyCombo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                getReverseEngineeringBySelectedMap().setNamingStrategy(
+                        (String) ReverseEngineeringConfigPanel.this.getStrategyCombo().getSelectedItem()
+                );
+                projectController.setDirty(true);
+            }
+        });
+        strategyCombo.setVisible(false);
+        meaningfulPk = new TextAdapter(new JTextField()) {
+            protected void updateModel(String text) {
+                getReverseEngineeringBySelectedMap().setMeaningfulPkTables(text);
+                projectController.setDirty(true);
+            }
+        };
+        meaningfulPk.getComponent().setToolTipText("<html>Regular expression to filter tables with meaningful primary keys.<br>" +
+                "Multiple expressions divided by comma can be used.<br>" +
+                "Example: <b>^table1|^table2|^prefix.*|table_name</b></html>");
+        stripFromTableNames = new TextAdapter(new JTextField()) {
+            protected void updateModel(String text) {
+                getReverseEngineeringBySelectedMap().setStripFromTableNames(text);
+                projectController.setDirty(true);
+            }
+        };
+        stripFromTableNames.getComponent().setToolTipText("<html>Regex that matches the part of the table name that needs to be stripped off " +
+                "when generating ObjEntity name</html>");
+
+        defaultPackage = new TextAdapter(new JTextField()) {
+            protected void updateModel(String text) {
+                getReverseEngineeringBySelectedMap().setDefaultPackage(text);
+                projectController.setDirty(true);
+            }
+        };
+        defaultPackage.getComponent().setToolTipText("<html>A Java package that will be set as the imported DataMap default and a package " +
+                "of all the persistent Java classes. This is a required attribute if the \"map\"<br> itself does not " +
+                "already contain a default package, as otherwise all the persistent classes will be mapped with no " +
+                "package, and will not compile.</html>");
+        skipRelationshipsLoading = new JCheckBox();
+        skipPrimaryKeyLoading = new JCheckBox();
+        forceDataMapCatalog = new JCheckBox();
+        forceDataMapSchema = new JCheckBox();
+        useJava7Types = new JCheckBox();
+        usePrimitives = new JCheckBox();
+        initStrategy();
+    }
+
+    private void initListeners() {
+        skipRelationshipsLoading.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                getReverseEngineeringBySelectedMap().setSkipRelationshipsLoading(skipRelationshipsLoading.isSelected());
+                projectController.setDirty(true);
+            }
+        });
+        skipPrimaryKeyLoading.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                getReverseEngineeringBySelectedMap().setSkipPrimaryKeyLoading(skipPrimaryKeyLoading.isSelected());
+                projectController.setDirty(true);
+            }
+        });
+        forceDataMapCatalog.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                getReverseEngineeringBySelectedMap().setForceDataMapCatalog(forceDataMapCatalog.isSelected());
+                projectController.setDirty(true);
+            }
+        });
+        forceDataMapSchema.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                getReverseEngineeringBySelectedMap().setForceDataMapSchema(forceDataMapSchema.isSelected());
+                projectController.setDirty(true);
+            }
+        });
+        usePrimitives.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                getReverseEngineeringBySelectedMap().setUsePrimitives(usePrimitives.isSelected());
+                projectController.setDirty(true);
+            }
+        });
+        useJava7Types.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                getReverseEngineeringBySelectedMap().setUseJava7Types(useJava7Types.isSelected());
+                projectController.setDirty(true);
+            }
+        });
+    }
+
+    JComboBox<String> getStrategyCombo() {
+        return strategyCombo;
+    }
+
+    TextAdapter getMeaningfulPk() {
+        return meaningfulPk;
+    }
+
+    TextAdapter getStripFromTableNames() {
+        return stripFromTableNames;
+    }
+
+    public TextAdapter getDefaultPackage() {
+        return defaultPackage;
+    }
+
+    JCheckBox getSkipRelationshipsLoading() {
+        return skipRelationshipsLoading;
+    }
+
+    JCheckBox getSkipPrimaryKeyLoading() {
+        return skipPrimaryKeyLoading;
+    }
+
+    JCheckBox getForceDataMapCatalog() {
+        return forceDataMapCatalog;
+    }
+
+    JCheckBox getForceDataMapSchema() {
+        return forceDataMapSchema;
+    }
+
+    JCheckBox getUsePrimitives() {
+        return usePrimitives;
+    }
+
+    JCheckBox getUseJava7Types() {
+        return useJava7Types;
+    }
+
+}
---
+++ modeler/cayenne-modeler/src/main/java/org/apache/cayenne/modeler/editor/TreeToolbarPanel.java
@@ -0,0 +1,200 @@
+/*****************************************************************
+ *   Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ ****************************************************************/
+
+package org.apache.cayenne.modeler.editor;
+
+import org.apache.cayenne.dbsync.reverse.dbimport.Catalog;
+import org.apache.cayenne.dbsync.reverse.dbimport.ExcludeColumn;
+import org.apache.cayenne.dbsync.reverse.dbimport.ExcludeProcedure;
+import org.apache.cayenne.dbsync.reverse.dbimport.ExcludeTable;
+import org.apache.cayenne.dbsync.reverse.dbimport.IncludeColumn;
+import org.apache.cayenne.dbsync.reverse.dbimport.IncludeProcedure;
+import org.apache.cayenne.dbsync.reverse.dbimport.IncludeTable;
+import org.apache.cayenne.dbsync.reverse.dbimport.ReverseEngineering;
+import org.apache.cayenne.dbsync.reverse.dbimport.Schema;
+import org.apache.cayenne.modeler.ProjectController;
+import org.apache.cayenne.modeler.action.AddCatalogAction;
+import org.apache.cayenne.modeler.action.AddExcludeColumnAction;
+import org.apache.cayenne.modeler.action.AddExcludeProcedureAction;
+import org.apache.cayenne.modeler.action.AddExcludeTableAction;
+import org.apache.cayenne.modeler.action.AddIncludeColumnAction;
+import org.apache.cayenne.modeler.action.AddIncludeProcedureAction;
+import org.apache.cayenne.modeler.action.AddIncludeTableAction;
+import org.apache.cayenne.modeler.action.AddPatternParamAction;
+import org.apache.cayenne.modeler.action.AddSchemaAction;
+import org.apache.cayenne.modeler.action.DeleteNodeAction;
+import org.apache.cayenne.modeler.action.EditNodeAction;
+import org.apache.cayenne.modeler.action.ReverseEngineeringAction;
+import org.apache.cayenne.modeler.action.TreeManipulationAction;
+import org.apache.cayenne.modeler.dialog.db.load.DbImportTreeNode;
+
+import javax.swing.JButton;
+import javax.swing.JToolBar;
+import javax.swing.JTree;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @since 4.1
+ */
+class TreeToolbarPanel extends JToolBar {
+
+    private static final int DEFAULT_LEVEL = -1;
+    private static final int FIRST_LEVEL = 0;
+    private static final int SECOND_LEVEL = 1;
+    private static final int THIRD_LEVEL = 5;
+    private static final int FOURTH_LEVEL = 7;
+
+    private JButton schemaButton;
+    private JButton catalogButton;
+    private JButton includeTableButton;
+    private JButton excludeTableButton;
+    private JButton includeColumnButton;
+    private JButton excludeColumnButton;
+    private JButton includeProcedureButton;
+    private JButton excludeProcedureButton;
+    private JButton editButton;
+    private JButton deleteButton;
+    private JButton generateButton;
+    private JTree reverseEngineeringTree;
+
+    private JButton[] buttons;
+    private Map<Class, Integer> levels;
+    private ProjectController projectController;
+    private ReverseEngineeringAction reverseEngineeringAction;
+
+    TreeToolbarPanel(ProjectController projectController, JTree reverseEngineeringTree) {
+        this.projectController = projectController;
+        this.reverseEngineeringTree = reverseEngineeringTree;
+        initLevels();
+        createButtons();
+        addButtons();
+        buttons = new JButton[]{catalogButton, schemaButton, includeTableButton, excludeTableButton,
+                includeProcedureButton, excludeProcedureButton ,includeColumnButton, excludeColumnButton};
+    }
+
+    void unlockButtons() {
+        changeToolbarButtonsState(false);
+        editButton.setEnabled(false);
+        deleteButton.setEnabled(false);
+    }
+
+    void lockButtons() {
+        if (reverseEngineeringTree.getLastSelectedPathComponent() != null) {
+            lockButtons(((DbImportTreeNode) reverseEngineeringTree.getLastSelectedPathComponent()).getUserObject());
+        } else {
+            changeToolbarButtonsState(true);
+            editButton.setEnabled(false);
+            deleteButton.setEnabled(false);
+        }
+    }
+
+    private void initLevels() {
+        levels = new HashMap<>();
+        levels.put(ReverseEngineering.class, DEFAULT_LEVEL);
+        levels.put(Catalog.class, FIRST_LEVEL);
+        levels.put(Schema.class, SECOND_LEVEL);
+        levels.put(IncludeTable.class, THIRD_LEVEL);
+        levels.put(ExcludeTable.class, FOURTH_LEVEL);
+        levels.put(IncludeColumn.class, FOURTH_LEVEL);
+        levels.put(ExcludeColumn.class, FOURTH_LEVEL);
+        levels.put(IncludeProcedure.class, FOURTH_LEVEL);
+        levels.put(ExcludeProcedure.class, FOURTH_LEVEL);
+    }
+
+    private void addButtons() {
+        this.setFloatable(false);
+        this.add(catalogButton);
+        this.add(schemaButton);
+        this.addSeparator();
+        this.add(includeTableButton);
+        this.add(excludeTableButton);
+        this.add(includeColumnButton);
+        this.add(excludeColumnButton);
+        this.add(includeProcedureButton);
+        this.add(excludeProcedureButton);
+        this.add(editButton);
+        this.addSeparator();
+        this.add(deleteButton);
+        this.addSeparator();
+        this.add(generateButton);
+    }
+
+    private void changeToolbarButtonsState(boolean state) {
+        schemaButton.setEnabled(state);
+        catalogButton.setEnabled(state);
+        includeTableButton.setEnabled(state);
+        excludeTableButton.setEnabled(state);
+        includeColumnButton.setEnabled(state);
+        excludeColumnButton.setEnabled(state);
+        includeProcedureButton.setEnabled(state);
+        excludeProcedureButton.setEnabled(state);
+        editButton.setEnabled(state);
+        deleteButton.setEnabled(state);
+    }
+
+    private void lockButtons(Object userObject) {
+        changeToolbarButtonsState(true);
+        if (levels.get(userObject.getClass()) == DEFAULT_LEVEL) {
+            editButton.setEnabled(false);
+            deleteButton.setEnabled(false);
+        }
+        lockButtonsOnLevel(levels.get(userObject.getClass()));
+    }
+
+    private void lockButtonsOnLevel(int level) {
+        for (int i = 0; i <= level; i++) {
+            buttons[i].setEnabled(false);
+        }
+    }
+
+    private <T extends TreeManipulationAction> JButton createButton(Class<T> actionClass, int position) {
+        TreeManipulationAction action = projectController.getApplication().getActionManager().getAction(actionClass);
+        action.setTree(reverseEngineeringTree);
+        return action.buildButton(position);
+    }
+
+    private <T extends AddPatternParamAction> JButton createButton(Class<T> actionClass, int position, Class paramClass) {
+        AddPatternParamAction action = projectController.getApplication().getActionManager().getAction(actionClass);
+        action.setTree(reverseEngineeringTree);
+        action.setParamClass(paramClass);
+        return action.buildButton(position);
+    }
+
+    private void createButtons() {
+        schemaButton = createButton(AddSchemaAction.class, 0);
+        catalogButton = createButton(AddCatalogAction.class, 0);
+        includeTableButton = createButton(AddIncludeTableAction.class, 1);
+        excludeTableButton = createButton(AddExcludeTableAction.class, 2, ExcludeTable.class);
+        includeColumnButton = createButton(AddIncludeColumnAction.class, 2, IncludeColumn.class);
+        excludeColumnButton = createButton(AddExcludeColumnAction.class, 2, ExcludeColumn.class);
+        includeProcedureButton = createButton(AddIncludeProcedureAction.class, 2, IncludeProcedure.class);
+        excludeProcedureButton = createButton(AddExcludeProcedureAction.class, 3, ExcludeProcedure.class);
+        editButton = createButton(EditNodeAction.class, 0);
+        deleteButton = createButton(DeleteNodeAction.class, 0);
+        reverseEngineeringAction = projectController.getApplication().getActionManager().
+                getAction(ReverseEngineeringAction.class);
+        reverseEngineeringAction.setView((DbImportView) this.getParent());
+        generateButton = reverseEngineeringAction.buildButton(0);
+    }
+
+    public void setParent(DbImportView parent) {
+        reverseEngineeringAction.setView(parent);
+    }
+}
